5a5c71370b3ecba27d9e483d95c01201f37135b4
dxl <1481451390@qq.com>
2020-09-10 10:07:21 +0800

    完成了登录和注册功能的开发

--- assets/js/login.js
+++ assets/js/login.js
@@ -11,6 +11,7 @@ $(function() {
 
     // 从 layui 中获取 form 对象   通过 form.verify() 函数自定义校验规则：
     var form = layui.form
+
     form.verify({
         pwd: [
             /^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'
@@ -27,5 +28,34 @@ $(function() {
 
         }
 
+    })
+
+    // 监听注册表单的提交事件
+    var layer = layui.layer
+    $('#form_reg').on('submit', function(e) {
+        e.preventDefault()
+        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form_reg [name=username]').val(), password: $('#form_reg [name=password]').val() }, function(res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功')
+            $('#link_login').click()
+        })
+    })
+    $('#form_login').submit(function(e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登陆成功')
+                localStorage.setItem('token', res.token)
+                    // location.href = '/index.html'
+            }
+        })
     })
 })
