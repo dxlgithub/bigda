46895f5d70b602a3651845806884139449c12e3b
dxl <1481451390@qq.com>
2020-09-13 15:36:30 +0800

    编辑完成

--- assets/js/aticle/art_cate.js
+++ assets/js/aticle/art_cate.js
@@ -1,5 +1,6 @@
 $(function() {
     var layer = layui.layer
+    var form = layui.form
 
     // 获取文章类别
     initArtCateList()
@@ -38,6 +39,40 @@ $(function() {
                 layer.msg('新增分类成功！')
                     // 根据索引，关闭对应的弹出层
                 layer.close(indexAdd)
+            }
+        })
+    })
+    $('tbody').on('click', '.btn-edit', function() {
+        // 弹出一个修改文章分类信息的层
+        indexEdit = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '修改文章分类',
+            content: $('#dialog-edit').html()
+        })
+        var id = $(this).attr('data-id')
+        console.log(id);
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates/' + id,
+            success: function(res) {
+                form.val('form-edit', res.data)
+            }
+        })
+    })
+    $('body').on('submit', '#form-edit', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/updatecate',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('编辑失败')
+                }
+                layer.msg('编辑成功')
+                layer.close(indexEdit)
+                initArtCateList()
             }
         })
     })
