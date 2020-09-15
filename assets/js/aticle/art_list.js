e7e7bb8bb28869a18dca93d613db22a3c53ae548
dxl <1481451390@qq.com>
2020-09-15 09:18:06 +0800

    文章列表和筛选

---
+++ assets/js/aticle/art_list.js
@@ -0,0 +1,77 @@
+$(function() {
+    layer = layui.layer
+    form = layui.form
+        // 定义美化时间的过滤器
+    template.defaults.imports.dataFormat = function(date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
+    }
+
+    // 定义补零的函数
+    function padZero(n) {
+        return n > 9 ? n : '0' + n
+    }
+    // 声明查询参数
+    var q = {
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
+    }
+    initTable()
+
+    function initTable() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/list',
+            data: q,
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取文章列表失败')
+                }
+                // console.log(res);
+                layer.msg('获取文章列表成功')
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+
+    initCate()
+
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败')
+                }
+                console.log(res)
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
+            }
+        })
+    }
+
+    $('#form-search').on('submit', function(e) {
+        e.preventDefault()
+
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+
+        q.cate_id = cate_id
+        q.state = state
+        initTable()
+    })
+})
